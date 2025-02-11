d3ad9a1faccebb0d1958a0101f6568d5689a7b94
Mikhail Kamochkin <mikhail.kamochkin@ontariotechu.net>
2025-02-11 11:39:24 -0500

    signin

--- Phase 2/Phase 2 Code/my_package/Main.py
+++ Phase 2/Phase 2 Code/my_package/Main.py
@@ -1,6 +1,10 @@
 # Main.py
 
 from User import User
+    
+    
+isAdmin = False
+backend_path = ETF.txt
 
 if __name__ == "__main__":
     # Input record (37 characters plus newline).
@@ -14,3 +18,21 @@ if __name__ == "__main__":
     print(account)
     # Expected output:
     # BankAccount(account_number=12345, account_holder='John Doe', status=A, balance=5431.0)
+
+
+def signin():
+    print("Welcome! Please select login type: (standard or admin)")
+    typeInput = input()
+    if (typeInput == "standard"):
+        isAdmin = False
+    elif (typeInput == "admin"):
+        isAdmin = True
+    else: (print("Error! Please enter 'standard' or 'admin'"))
+
+    if (isAdmin == False):
+        print("Standard session type seleted. Please enter your name:")
+        nameInput = input()
+        print("name:" + nameInput)
+
+
+    
