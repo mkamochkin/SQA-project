3965fc21ef4a84829ad655f62a58e0da2f43635a
Mikhail Kamochkin <mikhail.kamochkin@ontariotechu.net>
2025-02-11 11:44:51 -0500

    signin

--- Phase 2/Phase 2 Code/my_package/Main.py
+++ Phase 2/Phase 2 Code/my_package/Main.py
@@ -2,25 +2,11 @@
 
 from User import User
     
-    
-isAdmin = False
-backend_path = ETF.txt
-
-if __name__ == "__main__":
-    # Input record (37 characters plus newline).
-    # Note: Underscores represent spaces in the input.
-    sample_line = "12345_John_Doe_____________A_00005431\n"
-    
-    # Create a User (bank account) and load values from the sample line.
-    account = User()
-    account.load_from_line(sample_line)
-    
-    print(account)
-    # Expected output:
-    # BankAccount(account_number=12345, account_holder='John Doe', status=A, balance=5431.0)
-
+backend_path = "ETF.txt"
 
 def signin():
+    
+    isAdmin = False
     print("Welcome! Please select login type: (standard or admin)")
     typeInput = input()
     if (typeInput == "standard"):
@@ -33,6 +19,24 @@ def signin():
         print("Standard session type seleted. Please enter your name:")
         nameInput = input()
         print("name:" + nameInput)
+    else (isAdmin == True):
+        print("Welcome admin")
 
 
     
+
+if __name__ == "__main__":
+    # Input record (37 characters plus newline).
+    # Note: Underscores represent spaces in the input.
+    sample_line = "12345_John_Doe_____________A_00005431\n"
+    
+    # Create a User (bank account) and load values from the sample line.
+    account = User()
+    account.load_from_line(sample_line)
+    
+    print(account)
+
+    signin()
+    # Expected output:
+    # BankAccount(account_number=12345, account_holder='John Doe', status=A, balance=5431.0)
+
